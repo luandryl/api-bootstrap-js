a79c4a9290c9f997d11979334e7466131f7b2090
Luan Andryl <Luan.andryl@gmail.com>
2017-02-27 19:32:28 -0300

    tests pass/babel working

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import Square from './geometrics/Square'
 import Circle from './geometrics/Circle'
 import Triangle from './geometrics/Triangle'
 
-
 let s = new Square(4)
 let c = new Circle(1)
 let t = new Triangle(2, 4)
@@ -10,3 +9,4 @@ let t = new Triangle(2, 4)
 console.log(s.area())
 console.log(c.area())
 console.log(t.area())
+

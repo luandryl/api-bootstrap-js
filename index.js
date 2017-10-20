3b6cc5b54f221dc9a286fb9359f53b658deec947
luandryl <luan.andryl@gmail.com>
2017-10-20 05:44:21 -0200

    eita

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
-import express from 'express'
-import area from './routes/area'
-let app = express()
-app.set('port', (process.env.PORT || 5000))
+var express = require('express');
+var path = require('path');
+var serverStatic = require('serve-static');
 
-app.use('/', area)
+var app = express();
+
+app.use(serverStatic(__dirname));
 
 var port = process.env.PORT || 5000;
-
 app.listen(port);
 console.log('server started '+ port);
 
-export default app
+
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
